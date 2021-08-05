f636de97db7618c40c00e7dbaceeb4bdea530e03
Michael <michael007.ferr@gmail.com>
2021-08-06 02:16:38 +0530

    set up webpack and test bundle.js file

--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,34 @@
 const path = require('path'); // import Node.js path module for path related operations
 // create our config object
 const config = {
-entry: path.join(__dirname, '/client/index.js'), // Abosolute path to our entry file
-output: { // our output configuration
-path: path.join(__dirname, './public/'), // output path(directory/folder)
-filename: 'bundle.js' // output bundled file name
-},
-module: { // define our loaders here
-rules: [ // array of rules to handle different file types
-{
-test: /\.(js|jsx)$/, // check for .js and .jsx files (uses Regex)
-
-loader: 'babel-loader' // use these loaders for .js and .jsx files found
-}
-]
+    entry: path.join(__dirname, '/client/index.js'), // Abosolute pathto our entry file
+    output: { // our output configuration
+    path: path.join(__dirname, './public/'), // output path(directory/folder)
+    filename: 'bundle.js' // output bundled file name
+    },
+    module: { // define our loaders here
+    // array of rules to handle different file types
+    rules: [
+    {
+    test: /\.(js|jsx)$/, // check for .js and .jsx files (usesRegex)
+    
+    loader: 'babel-loader' // use this loader for .js and .jsxfiles found
+    },
+    {
+    // check for files ending with .css (uses Regex)
+    test: /\.css$/,
+    // use these loaders of .css files. 'css-loader gets runfirst and is
+    
+    // used to handle the imports of our css files inside ourjsx files.
+    
+    // The style loader then mounts our css in to the DOM
+    use: ['style-loader', 'css-loader']
+    }
+    ]
+    },
+    // set the file extensions we want webpack to resolve
+    resolve: {
+    extensions: ['.js', '.jsx']
 }
 };
 
